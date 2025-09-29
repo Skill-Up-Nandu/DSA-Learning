dbb1e380ba15522c7a33c2fff5e3ec40d39c326a
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2025-09-29 18:21:53 +0530

    Linear Search algorith

--- Linear.java
+++ Linear.java
@@ -1,32 +1,40 @@
 import java.util.*;
 
+// class of having searching logic
 public class Linear{
 
+    // fun for linear search
     public static int search(int[] arr , int x){
 
         // loop for search
         for(int i=0; i<arr.length; i++){
 
+            // return the index of key
             if (arr[i] == x){
                 return i;
             }
         }
+        // if not found return 
         return -1;
 
     }
 
 
     public static void main(){
+        
+        // initialize an array
         int[] arr = {5,8,9,63,45,12,01,77,56};
         System.out.println("Array : "+ Arrays.toString(arr));
 
+        // get key
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Element for Search : ");
         int x = sc.nextInt();
 
+        // function call
         int result = search(arr , x);
 
+        // print the result
         if (result == -1){
             System.out.println("Element "+ x + " not found !");
         }else{
