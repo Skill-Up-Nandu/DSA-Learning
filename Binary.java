c3c35cfab0fa64e9ae5277c4aef2fe3c14f11caa
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2025-09-29 21:32:32 +0530

    binary search of an array

---
+++ Binary.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Binary {
+
+    // function having login of binary search
+    public static int logic(int[] arr, int start, int end , int key){
+
+        // loop for finding value
+        while ( start <= end){
+
+            // middle element of array
+            int mid = ( start + end )/2;
+
+            // conditions to find exact element
+            if (key > arr[mid]){
+                start = mid+1;
+            }
+            else if (key < arr[mid]) {
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+
+        // return if not found
+        return -1;  
+    }
+
+    public static void main(String[] args) {
+
+        // initialize array
+        int[] arr = { 12, 36, 45, 51, 66, 71, 84, 99, 121 };
+        System.out.println("Array : " + Arrays.toString(arr));
+
+        // get key
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Key : ");
+        int key = sc.nextInt();
+
+        // function call
+        int result = logic(arr, 0, arr.length - 1, key);
+
+        // print result
+        if (result == -1) {
+            System.out.println("Element " + key + " not found.");
+        } else {
+            System.out.println("Element " + key + " found at index : " + result);
+        }
+    }
+}
